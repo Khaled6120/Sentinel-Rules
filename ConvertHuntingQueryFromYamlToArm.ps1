7bc4f342f7adbdedc59993ab1d5689986195255f
Khaled Naes <86200305+Khaled6120@users.noreply.github.com>
2024-06-25 12:47:56 +0300

    Update ConvertHuntingQueryFromYamlToArm.ps1

--- ConvertHuntingQueryFromYamlToArm.ps1
+++ ConvertHuntingQueryFromYamlToArm.ps1
@@ -66,42 +66,21 @@ function ConvertHuntingQueryFromYamlToArm {
         $huntingQueryObj.properties.tags += $descriptionObj
         $huntingQueryDescription = "$huntingQueryDescription "
     }
-    $tacticsMapping = @{
-    'reconnaissance' = 'Reconnaissance';
-    'resource_development' = 'Resource Development';
-    'initial_access' = 'Initial Access';
-    'execution' = 'Execution';
-    'persistence' = 'Persistence';
-    'privilege_escalation' = 'Privilege Escalation';
-    'defense_evasion' = 'Defense Evasion';
-    'credential_access' = 'Credential Access';
-    'discovery' = 'Discovery';
-    'collection' = 'Collection';
-    'command_and_control' = 'Command And Control';
-    'impact' = 'Impact';
-    'lateral_movement' = 'Lateral Movement';
-    'evasion' = 'Evasion';
-    'inhibit_response_function' = 'Inhibit Response Function';
-    'impair_process_control' = 'Impair Process Control'
-    }
-
     if ($yaml.tactics -and $yaml.tactics.Count -gt 0) {
-        $formattedTactics = $yaml.tactics | ForEach-Object {
-            $tactic = $_.Trim().ToLower()
-            if ($tacticsMapping.ContainsKey($tactic)) {
-                $tacticsMapping[$tactic]
-            } else {
-                throw "Invalid tactic: $tactic"
-            }
-        } -join ","
-       
         $tacticsObj = [PSCustomObject]@{
             name  = "tactics";
-            value = $formattedTactics
+            value = $yaml.tactics -join ","
         }
-    
+        if ($tacticsObj.value.ToString() -match ' ') {
+            $tacticsObj.value = $tacticsObj.value -replace ' ', ''
+        }
         $huntingQueryObj.properties.tags += $tacticsObj
     }
+
+
+
+
+
 
     if ($yaml.relevantTechniques -and $yaml.relevantTechniques.Count -gt 0) {
         $formattedTechniques = $yaml.relevantTechniques | ForEach-Object {
